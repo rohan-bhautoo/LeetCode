abc46c69c103676504cd5a31d4c0eb42cddadc66
Rohan Bhautoo <47154593+rohan-bhautoo@users.noreply.github.com>
2024-05-15 01:08:16 +0400

    Update Solution.cs

--- Valid Palindrome/Solution.cs
+++ Valid Palindrome/Solution.cs
@@ -1,1 +1,21 @@
+public class Solution 
+{
+    public bool IsPalindrome(string s) 
+    {
+      string cleanedString = Regex.Replace(s.ToLower(), "[^a-z0-9]", "");
 
+      int left = 0;
+      int right = cleanedString.Length - 1;
+
+      while(left < right)
+      {
+        if(cleanedString[left] != cleanedString[right])
+          return false;
+
+        left++;
+        right--;
+      }
+
+      return true;
+    }
+}
